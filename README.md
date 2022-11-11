c57e1f1a828dee0ea835005332fe2a188282b1b7
Tyler McCarthy <tweedlitweedli@gmail.com>
2022-11-11 12:30:07 -0600

    initial commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# Simple Signaling and Presence Server
+
+Allows for simple two-per-room matchmaking and SDP and ICE candidate signaling for creating a peer connection between two users. Only works in browsers with webRTC functionality.
+
+## Example
+
+`index.html` has an example where you can create a room and join an available room. After joining a room a RTCPeerConnection with a RTCDataChannel is established and a ping interval is set up to send and receive ping/pong messages every second.
+
+## Basic Flow
+
+Alice creates a room, is host
+Bob joins the room, is "peer"
+Alice gets alerted that a peer joined, sends an SDP offer
+    Alice also opens a data channel
+Bob gets the offer, sends an SDP answer
+(They both send ice candidates throughout)
+Once connected, they can send messages over the data channel
+    Connection is currently determined when the data channels open
+
+## TODOs
+[] reconnect logic
+[] joining named rooms
+[] presence updates
---
+++ main.js
@@ -0,0 +1,110 @@
+import { WebSocketServer } from 'ws'
+import { v4 as uuid } from 'uuid'
+
+const PORT = 6969
+
+let rooms = {}
+let users = {}
+
+const sendMessage = (ws, type, payload) => {
+    ws.send(JSON.stringify({ type, payload }))
+}
+
+// User methods
+const createUser = (ws) => {
+    const id = uuid()
+    const user = { id, ws }
+    users[id] = user
+    console.log('created user', user.id)
+    return user
+}
+
+const removeUser = (user) => {
+    delete users[user.id]
+    // TODO: remove user from rooms they may be in
+    console.log('removed user', user.id)
+}
+
+// Room methods
+const createRoom = (initialUser) => {
+    const id = uuid()
+    const room = { id, host: initialUser, peer: null }
+    rooms[id] = room
+    console.log('created room', id);
+    return room
+}
+
+// Connection methods
+const handleError = (e) => {
+    console.error(e)
+}
+
+const handleDisconnect = (user, code) => {
+    console.log(`user ${user.id} disconnected, code: ${code}`)
+    removeUser(user)
+}
+
+const handleMessage = ({ type, payload }, user) => {
+    console.log('message', type, payload)
+    switch (type) {
+        case 'create-room':
+            const room = createRoom(user)
+            sendMessage(user.ws, 'room-created', room.id)
+            break
+        case 'join-any-room':
+            let joined = false
+            for (let rid in rooms) {
+                const room = rooms[rid]
+                if (!room.peer) {
+                    room.peer = user
+                    joined = true
+                    sendMessage(room.host.ws, 'peer-joined', user.id)
+                    sendMessage(user.ws, 'joined-room', room.id)
+                    break
+                }
+            }
+            if (!joined) {
+                console.warn('could not join room', user.id)
+            }
+            break
+        // TODO:
+        case 'join-named-room': break
+        case 'sdp-offer':
+            const { peer } = rooms[payload.roomId]
+            sendMessage(peer.ws, 'sdp-offer', payload.offer)
+            break
+        case 'sdp-answer':
+            const { host } = rooms[payload.roomId]
+            sendMessage(host.ws, 'sdp-answer', payload.answer)
+            break
+        case 'ice-candidate':
+            const toRoom = rooms[payload.roomId]
+            const toUser = toRoom.host.id === user.id ? toRoom.peer : toRoom.host
+            sendMessage(toUser.ws, 'ice-candidate', payload.candidate)
+            break
+        // TODO: requires an old user id and a new user id
+        // we remove the new user and put this user object
+        // in the previous objects places
+        case 'reconnect':
+            break
+        default: console.warn('unhandled message', { type, payload })
+    }
+}
+
+const handleConnection = (ws) => {
+    const user = createUser(ws)
+    const messageHandler = (data) => handleMessage(JSON.parse(data.toString()), user)
+    ws.on('message', messageHandler)
+    ws.on('error', (e) => handleError(e))
+    ws.on('close', (code) => {
+        // TODO: destroy
+        ws.off('message', messageHandler)
+        handleDisconnect(user, code)
+    })
+    console.log(Object.keys(users))
+}
+
+const wss = new WebSocketServer({ port: PORT })
+wss.on('connection', (ws) => handleConnection(ws))
+
+console.log(`Websocket server listening on port ${PORT}`)
